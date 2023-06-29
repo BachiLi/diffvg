0d689f6045edadbaffda43c09a41d2b657e10306
Francisco Ibarrola <fibarrola@sinc.unl.edu.ar>
2023-06-29 16:20:23 +1000

    working

---
+++ apps/utils.py
@@ -0,0 +1,32 @@
+# Print iterations progress
+def printProgressBar(
+    iteration,
+    total,
+    loss,
+    prefix='Progress',
+    suffix='-- Loss: ',
+    decimals=1,
+    length=50,
+    fill='█',
+    printEnd="\r",
+):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    loss = "{:3f}".format(loss)
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix} {loss}', end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
